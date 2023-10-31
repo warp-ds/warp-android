606bbd7e0eef8e2018a03160309c0f93f51ebe27
julita.barkauskaite@schibsted.com <julita.barkauskaite@schibsted.com>
2023-10-31 11:02:30 +0100

    Tori color updates

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -5,7 +5,7 @@ object ConfigData {
     const val groupId = "com.schibsted.nmp.warp"
     const val versionWarp = "0.0.4"
     const val versionFinn = "0.0.5"
-    const val versionTori = "0.0.5"
+    const val versionTori = "0.0.6"
     const val artifactIdWarp = "warp-android"
     const val artifactIdFinn = "warp-android-finn"
     const val artifactIdTori = "warp-android-tori"
