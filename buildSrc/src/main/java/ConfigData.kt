3727424c6dc3990d7292850307ef1da77a8ad4a6
julita.barkauskaite@schibsted.com <julita.barkauskaite@schibsted.com>
2024-01-24 11:18:55 +0100

    Increment warp lib versions

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -3,9 +3,9 @@ object ConfigData {
     const val buildToolsVersion = "33.0.0"
     const val minSdkVersion = 24
     const val groupId = "com.schibsted.nmp.warp"
-    const val versionWarp = "0.0.11"
-    const val versionFinn = "0.0.12"
-    const val versionTori = "0.0.13"
+    const val versionWarp = "0.0.12"
+    const val versionFinn = "0.0.13"
+    const val versionTori = "0.0.14"
     const val artifactIdWarp = "warp-android"
     const val artifactIdFinn = "warp-android-finn"
     const val artifactIdTori = "warp-android-tori"
