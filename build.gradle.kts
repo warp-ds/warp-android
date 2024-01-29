81d29f96df99f3148045a9af5a26ad786bf9c245
julita.barkauskaite@schibsted.com <julita.barkauskaite@schibsted.com>
2024-01-29 18:41:50 +0100

    Add Firebase integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application") version "8.2.1" apply false
     id("com.android.library") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
     id("maven-publish")
 }
 apply(plugin = "com.jfrog.artifactory")
