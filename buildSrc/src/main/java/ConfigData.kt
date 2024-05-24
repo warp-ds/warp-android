b2700cee50f09a962fec99b5d02066d2d81dd3cd
julita.barkauskaite@schibsted.com <julita.barkauskaite@schibsted.com>
2024-05-24 14:16:00 +0200

    Increment version

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -2,7 +2,7 @@ object ConfigData {
     const val compileSdkVersion = 33
     const val buildToolsVersion = "33.0.0"
     const val minSdkVersion = 24
-    const val warpVersion = "0.0.22"
+    const val warpVersion = "0.0.23"
     const val groupId = "com.schibsted.nmp.warp"
     const val artifactIdWarp = "warp-android"
     const val artifactIdFinn = "warp-android-finn"
