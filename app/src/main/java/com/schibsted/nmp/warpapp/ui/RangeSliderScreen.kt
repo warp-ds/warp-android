488cd2680afe5130002f4d0e599c5f9ecd198b13
julbar <juli.bark@gmail.com>
2025-01-23 16:38:54 +0100

    Showcase int & string

--- app/src/main/java/com/schibsted/nmp/warpapp/ui/RangeSliderScreen.kt
+++ app/src/main/java/com/schibsted/nmp/warpapp/ui/RangeSliderScreen.kt
@@ -2,13 +2,18 @@ package com.schibsted.nmp.warpapp.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.schibsted.nmp.warp.components.WarpRangeSlider
+import com.schibsted.nmp.warp.components.WarpText
+import com.schibsted.nmp.warp.components.WarpTextStyle
 import com.schibsted.nmp.warp.theme.WarpTheme.dimensions
 
 @Composable
@@ -29,14 +34,49 @@ fun RangeSliderScreenContent() {
     Column(
         modifier = Modifier
             .padding(dimensions.space2)
-            .fillMaxSize(),
+            ,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
+        var from by remember { mutableStateOf(items.first().toString()) }
+        var to by remember { mutableStateOf(items.last().toString()) }
+        WarpText("Range slider with integers", style = WarpTextStyle.Title3, modifier = Modifier.padding(bottom = dimensions.space1))
+        WarpText("From $from to $to")
         WarpRangeSlider(
-            items = items
-        )
+            items = items,
+            onRightValueChanged = {
+                to = it.toString()
+            },
+            onLeftValueChanged = {
+                from = it.toString()
+            },
+            onValueChangeFinished = {
 
+            }
+        )
+        val stringItems = listOf("1900", "1910", "1920", "1930", "1940", "1950", "1960", "1970", "1980", "1990", "2000", "2010", "2020")
+        val startItem = stringItems[5]
+        val endItem = stringItems[8]
+        var from2 by remember { mutableStateOf(startItem) }
+        var to2 by remember { mutableStateOf(endItem) }
+        WarpText("Range slider with strings", style = WarpTextStyle.Title3, modifier = Modifier.padding(bottom = dimensions.space1))
+        WarpText("From $from2 to $to2")
+        WarpRangeSlider(
+            modifier = Modifier.padding(bottom = dimensions.space1),
+            items = stringItems,
+            initialStartItem = startItem,
+            initialEndItem = endItem,
+            onRightValueChanged = {
+                to2 = it.toString()
+            },
+            onLeftValueChanged = {
+                from2 = it.toString()
+            },
+            onValueChangeFinished = {
+
+            }
+        )
+        WarpText("Disabled range slider", style = WarpTextStyle.Title3, modifier = Modifier.padding(bottom = dimensions.space1))
         WarpRangeSlider(
             items = items,
             enabled = false,
