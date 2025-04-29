59beea05d8b6e31e2f7b43cf824d60cd1109b38d
julbar <juli.bark@gmail.com>
2025-04-29 15:05:48 +0200

    Increment version due to artifactory error

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,8 +1,8 @@
 object ConfigData {
     const val compileSdkVersion = 34
     const val minSdkVersion = 24
-    const val warpVersion = "0.0.41"
-    const val sampleAppVersionCode = 41
+    const val warpVersion = "0.0.42"
+    const val sampleAppVersionCode = 42
     const val groupId = "com.schibsted.nmp.warp"
     const val artifactIdWarp = "warp-android"
     const val artifactIdFinn = "warp-android-finn"
