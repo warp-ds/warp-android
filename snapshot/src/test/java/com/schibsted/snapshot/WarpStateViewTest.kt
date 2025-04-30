0abba555251889115ae2f07ac527ff094902382e
vladislav.georgiev.alfredov@schibsted.com <vladislav.georgiev.alfredov@schibsted.com>
2025-04-30 15:32:17 +0200

    Add snapshot testing

---
+++ snapshot/src/test/java/com/schibsted/snapshot/WarpStateViewTest.kt
@@ -0,0 +1,73 @@
+package com.schibsted.snapshot
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.HtmlReportWriter
+import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.SnapshotVerifier
+import com.android.ide.common.rendering.api.SessionParams
+import com.android.resources.NightMode
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.schibsted.nmp.warp.R
+import com.schibsted.nmp.warp.components.states.StateView
+import com.schibsted.nmp.warp.theme.WarpTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+ @RunWith(TestParameterInjector::class)
+class WarpStateViewTest(
+    @TestParameter val flavor: Flavor,
+    @TestParameter val nightMode: NightMode
+) {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_5.copy(
+            nightMode = nightMode
+        ),
+        validateAccessibility = true,
+        theme = "android:Theme.Material.Light.NoActionBar",
+        renderingMode = SessionParams.RenderingMode.SHRINK,
+        snapshotHandler = if (Config.isVerifying) {
+            SnapshotVerifier(
+                maxPercentDifference = Config.maxPercentDifference,
+                rootDirectory = flavor.dir
+            )
+        } else {
+            HtmlReportWriter(snapshotRootDirectory = flavor.dir)
+        }
+    )
+
+    @Test
+    fun warpStateView() {
+        paparazzi.snapshot {
+            WarpTheme(flavor = flavor) {
+                Column(
+                    modifier = Modifier
+                        .background(WarpTheme.colors.surface.elevated100)
+                        .fillMaxWidth()
+                ) {
+                    StateView(
+                        modifier = Modifier.fillMaxSize(),
+                        painter = painterResource(R.drawable.sparkles),
+                        imageContentDescription = stringResource(R.string.sparkles),
+                        title = "No data",
+                        description = "No data available",
+                        primaryButtonText = "Retry",
+                        onPrimaryButtonClicked = {},
+                        secondaryButtonText = "Get something random",
+                        onSecondaryButtonClicked = {},
+                    )
+                }
+
+            }
+        }
+    }
+}
