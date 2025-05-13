a3400f7fe0756b325dc3544dff22c3f243a9ca70
vladislav.georgiev.alfredov@schibsted.com <vladislav.georgiev.alfredov@schibsted.com>
2025-05-13 13:50:58 +0200

    Use WarpSpinner for LoadingWarpState

--- warp/src/main/java/com/schibsted/nmp/warp/components/states/LoadingWarpState.kt
+++ warp/src/main/java/com/schibsted/nmp/warp/components/states/LoadingWarpState.kt
@@ -2,12 +2,13 @@ package com.schibsted.nmp.warp.components.states
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.schibsted.nmp.warp.components.WarpSpinner
+import com.schibsted.nmp.warp.components.WarpSpinnerSize
 import com.schibsted.nmp.warp.theme.WarpTheme
 
 @Composable
@@ -16,10 +17,9 @@ fun LoadingWarpState(
     color: Color = WarpTheme.colors.background.secondary,
 ) {
     Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center
+        modifier = modifier, contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator(color = color)
+        WarpSpinner(size = WarpSpinnerSize.Large)
     }
 }
 
