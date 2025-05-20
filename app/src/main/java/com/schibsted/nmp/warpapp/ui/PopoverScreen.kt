105cb788ebc0deb3fd878c8db318097fee64efed
julbar <juli.bark@gmail.com>
2025-05-20 16:11:54 +0200

    Add a lorem ipsum text

--- app/src/main/java/com/schibsted/nmp/warpapp/ui/PopoverScreen.kt
+++ app/src/main/java/com/schibsted/nmp/warpapp/ui/PopoverScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import com.schibsted.nmp.warp.components.WarpButton
 import com.schibsted.nmp.warp.components.WarpPopover
 import com.schibsted.nmp.warp.components.WarpPopoverState
+import com.schibsted.nmp.warp.components.WarpText
 import com.schibsted.nmp.warp.components.utils.Edge
 import com.schibsted.nmp.warp.theme.WarpTheme.dimensions
 
@@ -52,6 +53,8 @@ fun PopoverScreenContent() {
             ),
             modifier = Modifier.fillMaxWidth()
         ) {
+            WarpText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
             //Top
             WarpPopover(
                 modifier = Modifier
