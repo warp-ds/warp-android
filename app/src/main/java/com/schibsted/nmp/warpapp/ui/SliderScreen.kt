7d3891f378bcb00c30e58d716ef03930fbd40ff4
julbar <juli.bark@gmail.com>
2025-06-02 17:25:16 +0200

    Add basic Slider implementation

--- app/src/main/java/com/schibsted/nmp/warpapp/ui/SliderScreen.kt
+++ app/src/main/java/com/schibsted/nmp/warpapp/ui/SliderScreen.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.schibsted.nmp.warp.components.WarpSlider
-import com.schibsted.nmp.warp.components.WarpText
-import com.schibsted.nmp.warp.components.WarpTextStyle
 import com.schibsted.nmp.warp.theme.WarpTheme.dimensions
 
 @Composable
@@ -27,30 +25,15 @@ fun SliderScreen(onUp: () -> Unit) {
 
 @Composable
 fun SliderScreenContent() {
-    val items = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
     Column(
         modifier = Modifier
             .padding(dimensions.space3),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        var from by remember { mutableStateOf(items.first().toString()) }
-        WarpText(
-            "Slider with integers",
-            style = WarpTextStyle.Title3,
-            modifier = Modifier.padding(bottom = dimensions.space1)
-        )
-        WarpText("From $from")
+        var value by remember { mutableFloatStateOf(.5f) }
         WarpSlider(
-            showTooltip = false,
-            items = items,
-            onValueChanged = {
-                from = it.toString()
-            },
-            onValueChangeFinished = {
-
-            },
-            showRange = false
-        )
+            value = value,
+            onValueChange = { value = it })
     }
 }
--- warp/src/main/java/com/schibsted/nmp/warp/components/WarpSlider.kt
+++ warp/src/main/java/com/schibsted/nmp/warp/components/WarpSlider.kt
@@ -1,35 +1,122 @@
 package com.schibsted.nmp.warp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalRippleConfiguration
+import androidx.compose.material3.RippleConfiguration
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderColors
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.schibsted.nmp.warp.theme.WarpTheme
+import com.schibsted.nmp.warp.theme.WarpTheme.colors
+import com.schibsted.nmp.warp.theme.WarpTheme.dimensions
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WarpSlider(
     modifier: Modifier = Modifier,
-    items: List<Any>,
+    value: Float,
+    onValueChange: (Float) -> Unit,
     enabled: Boolean = true,
-    initialStartItem: Any? = null,
-    onValueChangeFinished: (() -> Unit) = {},
-    onValueChanged: ((Any) -> Unit) = {},
-    blockDrag: Boolean = false,
+    onValueChangeFinished: (() -> Unit)? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    /*
     showTooltip: Boolean = true,
     showRange: Boolean = false,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    ,*/
 ) {
-    WarpRangeSlider(
-        modifier = modifier,
-        items = items,
-        enabled = enabled,
-        initialStartItem = initialStartItem,
-        onValueChangeFinished = onValueChangeFinished,
-        onLeftValueChanged = onValueChanged,
-        blockDrag = blockDrag,
-        showTooltips = showTooltip,
-        showRange = showRange,
-        startInteractionSource = interactionSource
-    )
+    val rippleConfiguration =
+        RippleConfiguration(color = Color.Black, rippleAlpha = RippleAlpha(0.1f, 0f, 0f, 0.2f))
+    CompositionLocalProvider(
+        LocalRippleConfiguration provides rippleConfiguration
+    ) {
+        Slider(
+            modifier = modifier,
+            value = value,
+            onValueChange = onValueChange,
+            enabled = enabled,
+            onValueChangeFinished = onValueChangeFinished,
+            interactionSource = interactionSource,
+            colors = warpSliderColors,
+            thumb = {
+                Spacer(
+                    Modifier
+                        .size(dimensions.components.slider.thumbSize)
+                        .indication(
+                            interactionSource = interactionSource,
+                            indication = ripple(
+                                bounded = false,
+                                radius = 45.dp / 2
+                            )
+                        )
+
+                        .background(
+                            warpSliderColors.thumbColor,
+                            WarpTheme.shapes.ellipse
+                        )
+                )
+            },
+            track = { sliderState ->
+
+                val fraction by remember {
+                    derivedStateOf {
+                        (sliderState.value - sliderState.valueRange.start) / (sliderState.valueRange.endInclusive - sliderState.valueRange.start)
+                    }
+                }
+
+                Box(Modifier.fillMaxWidth()) {
+                    Box(
+                        Modifier
+                            .fillMaxWidth(fraction)
+                            .align(Alignment.CenterStart)
+                            .height(dimensions.components.slider.activeTrackHeight)
+                            .background(warpSliderColors.activeTrackColor, CircleShape)
+                    )
+                    Box(
+                        Modifier
+                            .fillMaxWidth(1f - fraction)
+                            .align(Alignment.CenterEnd)
+                            .height(dimensions.components.slider.inactiveTrackHeight)
+                            .background(warpSliderColors.inactiveTrackColor, CircleShape)
+                    )
+                }
+            }
+
+        )
+    }
 }
+
+internal val warpSliderColors: SliderColors
+    @Composable
+    get() {
+        return SliderColors(
+            thumbColor = colors.background.primary,
+            activeTrackColor = colors.background.primary,
+            activeTickColor = colors.background.primary,
+            inactiveTrackColor = colors.background.disabledSubtle,
+            inactiveTickColor = colors.background.disabledSubtle,
+            disabledThumbColor = colors.background.disabledSubtle,
+            disabledActiveTrackColor = colors.background.disabledSubtle,
+            disabledActiveTickColor = colors.background.disabledSubtle,
+            disabledInactiveTrackColor = colors.background.disabledSubtle,
+            disabledInactiveTickColor = colors.background.disabledSubtle
+        )
+    }
