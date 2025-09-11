ee514bf9924e78a465ce842301196760f1c2eaba
julbar <juli.bark@gmail.com>
2025-09-11 16:44:54 +0200

    Add docked WarpDatePicker

--- app/src/main/java/com/schibsted/nmp/warpapp/ui/DatePickerScreen.kt
+++ app/src/main/java/com/schibsted/nmp/warpapp/ui/DatePickerScreen.kt
@@ -2,14 +2,20 @@ package com.schibsted.nmp.warpapp.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.schibsted.nmp.warp.components.WarpDatePicker
-import com.schibsted.nmp.warp.theme.WarpTheme.dimensions
+import com.schibsted.nmp.warp.components.WarpDatePickerType
+import com.schibsted.nmp.warp.components.WarpText
+import com.schibsted.nmp.warp.components.WarpTextStyle
+import java.util.Locale
 
 @Composable
 fun DatePickerScreen(onUp: () -> Unit) {
@@ -21,15 +27,27 @@ fun DatePickerScreen(onUp: () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerScreenContent() {
     Column(
-        modifier = Modifier
-            .padding(vertical = dimensions.space7, horizontal = dimensions.space2)
-            .verticalScroll(rememberScrollState()),
+        modifier = Modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        WarpDatePicker()
+        WarpText(text = "Docked type", style = WarpTextStyle.Title3)
+
+        var dateinMillis = System.currentTimeMillis()
+        val formatter = remember { DatePickerDefaults.dateFormatter() }
+        var date = formatter.formatDate(dateinMillis, Locale.getDefault())
+        var dateString by remember { mutableStateOf(date) }
+        WarpDatePicker(
+            type = WarpDatePickerType.DOCKED,
+            onDateSelected = {
+                dateinMillis = it
+                dateString = formatter.formatDate(dateinMillis, Locale.getDefault())
+            }
+        )
+        WarpText(text = "Selected date: ${dateString}")
     }
 }
--- warp/src/main/java/com/schibsted/nmp/warp/components/WarpDatePicker.kt
+++ warp/src/main/java/com/schibsted/nmp/warp/components/WarpDatePicker.kt
@@ -1,62 +1,116 @@
 package com.schibsted.nmp.warp.components
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerColors
 import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerFormatter
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.schibsted.nmp.warp.theme.WarpTheme.colors
-import java.util.Locale
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
+
+/**
+ * A date picker in the warp design system.
+ * @param modifier The modifier to be applied to the date picker.
+ * @param onDateSelected A callback that is invoked when a date is selected.
+ * @param preselectedDateMillis The preselected date in milliseconds. Todays date will be used if not provided
+ * @param type The type of the date picker. Defaults to [WarpDatePickerType.DIALOG].
+ * @param dateFormatter The date formatter to be used. Defaults to [DatePickerDefaults.dateFormatter].
+ * @param selectableDates The selectable dates to be used. Defaults to [DatePickerDefaults.AllDates].
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WarpDatePicker(
+    modifier: Modifier = Modifier,
+    onDateSelected: (Long) -> Unit,
+    preselectedDateMillis: Long? = null,
+    type: WarpDatePickerType = WarpDatePickerType.DIALOG,
+    dateFormatter: DatePickerFormatter = remember { DatePickerDefaults.dateFormatter() },
+    selectableDates: SelectableDates = DatePickerDefaults.AllDates
+) {
+    val displayMode = when (type) {
+        WarpDatePickerType.DIALOG -> {
+            DisplayMode.Picker
+        }
+
+        WarpDatePickerType.DOCKED -> {
+            DisplayMode.Picker
+        }
+
+        WarpDatePickerType.TEXTFIELD -> {
+            DisplayMode.Input
+        }
+    }
+
+    val datePickerState: DatePickerState = rememberDatePickerState(
+            initialSelectedDateMillis = preselectedDateMillis ?: System.currentTimeMillis(),
+            initialDisplayMode = displayMode,
+            selectableDates = selectableDates
+        )
+
+    //Keep track of the value change
+    LaunchedEffect(datePickerState) {
+        snapshotFlow { datePickerState.selectedDateMillis }
+            .filterNotNull()
+            .distinctUntilChanged()
+            .collect { newSelectedTimestamp ->
+                onDateSelected(newSelectedTimestamp)
+            }
+    }
+
+    DatePicker(
+        state = datePickerState,
+        modifier = modifier,
+        dateFormatter = dateFormatter,
+        title = title,
+        headline = headline,
+        showModeToggle = false,
+        colors = datePickerColors()
+    )
+}
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-fun WarpDatePicker() {
-    Locale.setDefault(Locale.GERMAN)
-    val datePickerState = rememberDatePickerState(initialSelectedDateMillis = 1578096000000)
-    DatePicker(state = datePickerState, modifier = Modifier.padding(4.dp),
-        colors = DatePickerDefaults.colors(
-            containerColor = colors.background.default,
-            titleContentColor = colors.text.default,
-            selectedDayContainerColor = colors.icon.primary,
-        ))
-    /**
-     * Investigate typeface
-     * Customizable stuff
-     */
-
-/*
-        containerColor: Color = Color.Unspecified,
-        titleContentColor: Color = Color.Unspecified,
-        headlineContentColor: Color = Color.Unspecified,
-        weekdayContentColor: Color = Color.Unspecified,
-        subheadContentColor: Color = Color.Unspecified,
-        navigationContentColor: Color = Color.Unspecified,
-        yearContentColor: Color = Color.Unspecified,
-        disabledYearContentColor: Color = Color.Unspecified,
-        currentYearContentColor: Color = Color.Unspecified,
-        selectedYearContentColor: Color = Color.Unspecified,
-        disabledSelectedYearContentColor: Color = Color.Unspecified,
-        selectedYearContainerColor: Color = Color.Unspecified,
-        disabledSelectedYearContainerColor: Color = Color.Unspecified,
-        dayContentColor: Color = Color.Unspecified,
-        disabledDayContentColor: Color = Color.Unspecified,
-        selectedDayContentColor: Color = Color.Unspecified,
-        disabledSelectedDayContentColor: Color = Color.Unspecified,
-        selectedDayContainerColor: Color = Color.Unspecified,
-        disabledSelectedDayContainerColor: Color = Color.Unspecified,
-        todayContentColor: Color = Color.Unspecified,
-        todayDateBorderColor: Color = Color.Unspecified,
-        dayInSelectionRangeContentColor: Color = Color.Unspecified,
-        dayInSelectionRangeContainerColor: Color = Color.Unspecified,
-        dividerColor: Color = Color.Unspecified,
-        dateTextFieldColors: TextFieldColors? = null
- */
-    Text(
-        "Selected date timestamp: ${datePickerState.selectedDateMillis ?: "no selection"}"
+private fun datePickerColors(): DatePickerColors = DatePickerDefaults
+    .colors(
+        containerColor = colors.background.default,
+        titleContentColor = colors.text.default,
+        headlineContentColor = colors.text.default,
+        weekdayContentColor = colors.text.default,
+        subheadContentColor = colors.text.default,
+        navigationContentColor = colors.text.default,
+        yearContentColor = colors.text.default,
+        dayContentColor = colors.text.default,
+        disabledYearContentColor = colors.text.disabled,
+        currentYearContentColor = colors.text.default,
+        selectedDayContainerColor = colors.background.primary,
+        selectedYearContentColor = colors.text.inverted,
+        selectedYearContainerColor = colors.background.primary,
+        selectedDayContentColor = colors.text.inverted,
+        disabledSelectedYearContentColor = colors.text.disabled,
+        disabledSelectedYearContainerColor = colors.background.disabled,
+        disabledSelectedDayContentColor = colors.text.inverted,
+        disabledSelectedDayContainerColor = colors.background.disabled,
+        disabledDayContentColor = colors.text.disabled,
+        todayContentColor = colors.text.default,
+        todayDateBorderColor = colors.border.focus,
+        dayInSelectionRangeContentColor = colors.text.default,
+        dayInSelectionRangeContainerColor = colors.background.primary,
+        dividerColor = colors.background.subtle,
+        dateTextFieldColors = warpTextFieldColors()
     )
 
+enum class WarpDatePickerType {
+    DIALOG,
+    DOCKED,
+    TEXTFIELD
 }
