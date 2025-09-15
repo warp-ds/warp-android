980931f2f4f9b746567dee3fa20225360b1a37ac
julbar <juli.bark@gmail.com>
2025-09-15 17:15:22 +0200

    Add selectable dates

--- app/src/main/java/com/schibsted/nmp/warpapp/ui/DatePickerScreen.kt
+++ app/src/main/java/com/schibsted/nmp/warpapp/ui/DatePickerScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -64,6 +65,13 @@ fun DatePickerScreenContent() {
             modifier = Modifier.padding(vertical = dimensions.space3)
         )
 
+        val futureDates = object  :
+            SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                return utcTimeMillis > System.currentTimeMillis()
+            }
+        }
+
         var showDialog by remember { mutableStateOf(false) }
         WarpTextField(
             modifier = Modifier.padding(horizontal = dimensions.space2),
@@ -81,7 +89,8 @@ fun DatePickerScreenContent() {
                     dateString = formatter.formatDate(dateinMillis, Locale.getDefault())
                     showDialog = false
                 },
-                onDismiss = { showDialog = !showDialog }
+                onDismiss = { showDialog = !showDialog },
+                selectableDates = futureDates
             )
         }
     }
