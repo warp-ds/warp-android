91d30e44aa4b65b77b42308c9983653c976679e0
julbar <juli.bark@gmail.com>
2025-11-17 16:42:21 +0100

    Increment version

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,8 +1,8 @@
 object ConfigData {
     const val compileSdkVersion = 34
     const val minSdkVersion = 24
-    const val warpVersion = "0.0.48"
-    const val sampleAppVersionCode = 48
+    const val warpVersion = "0.0.49"
+    const val sampleAppVersionCode = 49
     const val groupId = "com.schibsted.nmp.warp"
     const val artifactIdWarp = "warp-android"
     const val artifactIdFinn = "warp-android-finn"
