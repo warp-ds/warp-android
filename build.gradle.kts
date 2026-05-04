b29fafcdeb1bb2cee56ec62d2fafcd7121f80023
fredrik-kvitvik <fredrik.kvitvik@finn.no>
2026-05-04 11:27:03 +0200

    Make Artifactory plugin conditional for publishing only
    - Artifactory is now only loaded when -PPUBLISH_TO_ARTIFACTORY=true is passed
    - Regular builds (including GitHub Actions) no longer require Artifactory credentials
    - Updated release-warp.sh to enable Artifactory when publishing
    - Fixes Firebase App Distribution workflow build failure

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,48 +7,65 @@ plugins {
     id("maven-publish")
     id("app.cash.paparazzi") version "1.3.4" apply false
 }
-apply(plugin = "com.jfrog.artifactory")
 
+// Only apply Artifactory configuration when explicitly publishing
+// Usage: ./gradlew artifactoryPublish -PPUBLISH_TO_ARTIFACTORY=true
+val enableArtifactory = project.hasProperty("PUBLISH_TO_ARTIFACTORY")
+
+if (enableArtifactory) {
+    apply(plugin = "com.jfrog.artifactory")
+}
 
 buildscript {
-    Artifactory.userName = System.getenv("ARTIFACTORY_USER") ?: extra["ARTIFACTORY_USER"].toString()
-    Artifactory.password = System.getenv("ARTIFACTORY_PWD") ?: extra["ARTIFACTORY_PWD"].toString()
+    val enableArtifactory = project.hasProperty("PUBLISH_TO_ARTIFACTORY")
+
+    if (enableArtifactory) {
+        Artifactory.userName = System.getenv("ARTIFACTORY_USER") ?: extra["ARTIFACTORY_USER"].toString()
+        Artifactory.password = System.getenv("ARTIFACTORY_PWD") ?: extra["ARTIFACTORY_PWD"].toString()
+    }
 
     repositories {
         mavenCentral()
         maven { url = uri("https://plugins.gradle.org/m2/") }
         google()
-        maven {
-            url = uri("https://artifacts.schibsted.io/artifactory/plugins-release")
-            credentials {
-                username = System.getenv("ARTIFACTORY_USER") ?: extra["ARTIFACTORY_USER"].toString()
-                password = System.getenv("ARTIFACTORY_PWD") ?: extra["ARTIFACTORY_PWD"].toString()
+        if (enableArtifactory) {
+            maven {
+                url = uri("https://artifacts.schibsted.io/artifactory/plugins-release")
+                credentials {
+                    username = System.getenv("ARTIFACTORY_USER") ?: extra["ARTIFACTORY_USER"].toString()
+                    password = System.getenv("ARTIFACTORY_PWD") ?: extra["ARTIFACTORY_PWD"].toString()
+                }
             }
         }
     }
     dependencies {
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.4")
-        classpath("org.jfrog.buildinfo:build-info-extractor-gradle:5.1.6")
+        if (enableArtifactory) {
+            classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.4")
+            classpath("org.jfrog.buildinfo:build-info-extractor-gradle:5.1.6")
+        }
     }
 }
-configure<org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention> {
-    publish {
-        // Define the Artifactory URL for publishing artifacts
-        contextUrl = "https://artifacts.schibsted.io/artifactory"
-        // Define the project repository to which the artifacts will be published
-        repository {
-            // Set the Artifactory repository key
-            repoKey = "libs-release-local"
-            // Specify the publisher username
-            username = Artifactory.userName
-            // Provide the publisher password
-            password = Artifactory.password
 
-        }
+if (enableArtifactory) {
+    configure<org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention> {
+        publish {
+            // Define the Artifactory URL for publishing artifacts
+            contextUrl = "https://artifacts.schibsted.io/artifactory"
+            // Define the project repository to which the artifacts will be published
+            repository {
+                // Set the Artifactory repository key
+                repoKey = "libs-release-local"
+                // Specify the publisher username
+                username = Artifactory.userName
+                // Provide the publisher password
+                password = Artifactory.password
 
-        // Include all configured publications for all the modules
-        defaults {
-            publications("ALL_PUBLICATIONS")
+            }
+
+            // Include all configured publications for all the modules
+            defaults {
+                publications("ALL_PUBLICATIONS")
+            }
         }
     }
 }
