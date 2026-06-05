95e59af9c7b1135104baf4544c271d49c71cf086
fredrik-kvitvik <fredrik.kvitvik@finn.no>
2026-06-05 09:01:14 +0200

    Separate snapshot test modules for all components

---
+++ snapshot-rangeslider/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("app.cash.paparazzi")
+}
+
+android {
+    namespace = "com.schibsted.snapshot.rangeslider"
+    compileSdk = ConfigData.compileSdkVersion
+
+    defaultConfig {
+        minSdk = ConfigData.minSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+    testOptions.unitTests.isIncludeAndroidResources = true
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.kotlinCompiler
+    }
+}
+
+dependencies {
+    implementation(project(":snapshot-test-utils"))
+    implementation(Dependencies.composeMaterial3)
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
+}
